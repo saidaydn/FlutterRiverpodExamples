601f58c68f7de76c53c792202ae4091c08f89193
said aydin <saita2147@gmail.com>
2023-06-21 14:06:35 +0300

    fist commit

---
+++ lib/provider/provider.dart
@@ -0,0 +1,31 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+final randomValueProvider = Provider<int>((ref) {
+  final random = ref.watch(randomProvider);
+  return random.nextInt(100) +50;
+});
+
+final randomProvider = Provider<Random>((ref) => Random());
+
+class RandomValuePage extends ConsumerWidget {
+  const RandomValuePage({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final randomValue = ref.watch(randomValueProvider);
+    return Scaffold(
+      appBar: AppBar(
+        title: const  Text('Rastgele Değer'),
+      ),
+      body: Center(
+        child: Text(
+          'Rastgele Değer: $randomValue',
+          style: const  TextStyle(fontSize: 24),
+        ),
+      ),
+    );
+  }
+}
+
